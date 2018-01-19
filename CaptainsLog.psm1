5363f1f88cb14772e822c9a53845d5172d04cac0
Eric De Wildt <dewildte@gmail.com>
2018-01-19 09:26:22 -0500

    Templates now use proper markdown syntax

--- CaptainsLog.psm1
+++ CaptainsLog.psm1
@@ -1,23 +1,31 @@
 # The purpose of the momdule is to assist in the making of the captains logs
 
 
-$stardate = Get-Date -Format yyyyMMdd
+$stardate = Get-Date -Format yyyy-MM-dd-T-hh:mm
 
-$template = " 
-#Captains Log
-Star Date: $stardate
---------------------"
+$template = "## __Captains Log__
+__Star Date:__ $stardate
+*****************************
+
+"
+
+$supplament = "
+
+## __Supplamental__
+"
 
 
 Function New-CaptainsLog {
-
-	$fileName = "CPL.txt";
 
 	$year = Get-Date -Format yyyy;
 
 	$month = Get-Date -Format MM;
 
 	$day = Get-Date -Format dd;
+	
+	$date = "$year$month$day"
+
+	$fileName = "CPL$date.txt";
 
 	$file = "$HOME\captainsLog\$year\$month\$day\$fileName"
 	
@@ -28,4 +36,26 @@ Function New-CaptainsLog {
 	New-Item $file -Itemtype File
 
 	$template | Set-Content $file
+}
+
+Function Add-SupplamentToCaptainsLog {
+	
+	$year = Get-Date -Format yyyy;
+
+	$month = Get-Date -Format MM;
+
+	$day = Get-Date -Format dd;
+
+	$date = "$year$month$day"
+
+	$time = Get-Date -Format hh:mm
+
+	$fileName = "CPL$date.txt";
+
+	$supplamentalText = $supplament + $time + "
+***************
+
+"
+
+	$supplamentalText | Add-Content "$HOME\captainsLog\$year\$month\$day\$fileName"
 }
